5e1126a478d90a70f83b072228b80fdb8c626281
Pleket <50240262+Pleket@users.noreply.github.com>
2020-02-20 16:56:33 +0100

    Setup

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    Scanner scanner = new Scanner(System.in);
+
+    private void run() {
+        parse();
+
+    }
+
+    private void parse() {
+
+    }
+
+    public static void main(String[] args) {
+        Main solution = new Main();
+        solution.run();
+    }
+}

e376af2f1010af314fb4172b2a4d68218b9a9afa
Pleket <50240262+Pleket@users.noreply.github.com>
2020-02-20 20:24:33 +0100

    Output

--- src/Output.java
+++ src/Output.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
 public class Output {
-    public void output(int librariesSigned, ArrayList<Library> lib) {
+    public void output(int librariesSigned, ArrayList<Library> lib, ArrayList<Integer> bookAmounts, ArrayList<ArrayList<Integer>> bookIDs) {
         System.out.println(librariesSigned);
         for (int i = 0; i < librariesSigned; i++) {
-            
+            int id = lib.get(i).id;
+            System.out.println(lib.get(i).id + " " + bookAmounts.get(id));
+            for (int j = 0; j < bookAmounts.get(id) - 1; j++) {
+                System.out.print(bookIDs.get(id).get(j) + " ");
+            }
+            System.out.println(bookIDs.get(id).get(bookIDs.get(id).size()) - 1);
         }
     }
 }

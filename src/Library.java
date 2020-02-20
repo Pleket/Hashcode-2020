1e36b4bc339ffc2cd6c674e4d811491e34950dbb
Martijn Leus <m.h.m.leus@student.tue.nl>
2020-02-20 19:07:09 +0100

    Add vars.

--- src/Library.java
+++ src/Library.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 
 public class Library {
     ArrayList<Integer> books = new ArrayList<Integer>();
+    int days;
+    int scannable;
 
     public Library() { }
+
+
 
 }

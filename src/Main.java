aceb387fe021d3f7cdca1775123ce01bb188a9a9
Pleket <50240262+Pleket@users.noreply.github.com>
2020-02-20 19:02:00 +0100

    Parser

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Main {
     Scanner scanner = new Scanner(System.in);
 
+    ArrayList<Library> lib;
+
     private void run() {
         parse();
-
     }
 
     private void parse() {
+        Parser parser = new Parser();
 
+        lib = parser.getLibraries();
     }
 
     public static void main(String[] args) {
---
+++ src/Parser.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Parser {
+    int b;
+
+    int l;
+
+    int d;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public Parser() {
+        b = scanner.nextInt();
+        l = scanner.nextInt();
+        d = scanner.nextInt();
+    }
+
+    public ArrayList<Library> getLibraries() {
+        
+    }
+}

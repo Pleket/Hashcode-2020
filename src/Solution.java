be497cdc7171ecd9caf77fadc39aef748a47b52a
Martijn Leus <m.h.m.leus@student.tue.nl>
2020-02-20 19:29:52 +0100

    3

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Solution {
 
+    HashSet<Integer> scannedBooks = new HashSet<>();
 
+    public Solution(ArrayList<Library> libs, ArrayList<Integer> bookscores, int days) {
 
-    public Solution(ArrayList<Library> libs, ArrayList<Integer> bookscores) {
 
     }
 }
